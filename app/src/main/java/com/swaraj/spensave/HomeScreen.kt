f16d8d1ded8ad04d1c6eb01dd9a71bff12410ae4
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2024-09-01 17:09:18 +0530

    home page designed

--- app/src/main/java/com/swaraj/spensave/HomeScreen.kt
+++ app/src/main/java/com/swaraj/spensave/HomeScreen.kt
@@ -1,11 +1,33 @@
+@file:Suppress("PreviewMustBeTopLevelFunction")
+
 package com.swaraj.spensave
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.swaraj.spensave.ui.theme.Zinc
 
 @Composable
 fun HomeScreen() {
@@ -16,13 +38,208 @@ fun HomeScreen() {
             modifier = Modifier.fillMaxSize()
         ) {
             val(nameRow,list,card,topBar) = createRefs()
-
+            Image(painterResource(id = R.drawable.ic_topbar),
+                contentDescription = null,
+                modifier = Modifier.constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            Box (modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 64.dp, start = 16.dp, end = 16.dp)
+                .constrainAs(nameRow) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    //bottom.linkTo(parent.bottom)
+                }
+            ){
+                Column {
+                    Text(
+                        "hello bhyi",
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                    Text(
+                        "tutorial de sikhraha hai",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+                Image(
+                    painterResource(id = R.drawable.ic_notifications),
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                )
+            }
+            CardItem(modifier = Modifier
+                .constrainAs(card){
+                    top.linkTo(nameRow.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+//                    bottom.linkTo(nameRow.bottom)
+                }
+            )
+            TransactionList(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(list) {
+                        top.linkTo(card.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                        height = Dimension.fillToConstraints
+                    }
+            )
         }
     }
 }
 
 @Composable
-@Preview(showBackground = true)
-fun HomeScreenPreview() {
-    HomeScreen()
+fun CardItem(modifier: Modifier) {
+    Column(modifier = modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .height(200.dp)
+        .clip(RoundedCornerShape(16.dp))
+        .background(Zinc)
+        .padding(16.dp)
+    ) {
+        Box (modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+        ) {
+            Column(
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Text(text = "paisaa", fontSize = 16.sp, color = Color.White)
+                Text(
+                    text = "150 rupia",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+            Image(
+                painter = painterResource(id = R.drawable.dots_menu),
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            CardRowItem(
+                modifier = Modifier.align(Alignment.CenterStart),
+                title = "income",
+                amount = "kicchi nahi",
+                image = R.drawable.ic_income
+            )
+            CardRowItem(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                title = "kharcha",
+                amount = "bahut achhi",
+                image = R.drawable.ic_expense
+            )
+        }
+    }
 }
+
+@Composable
+fun CardRowItem(
+    modifier: Modifier,
+    title: String,
+    amount: String,
+    image: Int
+){
+    Column(modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = title, fontSize = 16.sp, color = Color.White)
+
+        }
+        Text(text = amount, fontSize = 20.sp, color = Color.White)
+    }
+}
+
+@Composable
+fun TransactionList(modifier: Modifier) {
+    Column(modifier = modifier.padding(horizontal = 16.dp)) {
+        Box (modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Recent Transactions", fontSize = 20.sp)
+            Text(
+                text = "See All",
+                fontSize = 16.sp,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+        TransactionItem(
+            title = "Netflix",
+            amount = "-199",
+            icon = R.drawable.ic_netflix,
+            date = "aji",
+            color = Color.Red
+        )
+        TransactionItem(
+            title = "Biriyani",
+            amount = "-150",
+            icon = R.drawable.ic_expense,
+            date = "aji",
+            color = Color.Red
+        )
+        TransactionItem(
+            title = "Salary",
+            amount = "+00",
+            icon = R.drawable.ic_income,
+            date = "aji",
+            color = Color.Green
+        )
+        TransactionItem(
+            title = "chai",
+            amount = "-20",
+            icon = R.drawable.ic_expense,
+            date = "kali",
+            color = Color.Red
+        )
+    }
+}
+
+@Composable
+fun TransactionItem (title: String, amount: String, icon: Int, date: String, color: Color) {
+    Box(modifier = Modifier.fillMaxWidth()
+        .padding(vertical = 8.dp)) {
+        Row {
+            Image(
+                painterResource(id = icon), contentDescription = null,
+                modifier = Modifier.size(50.dp)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(text = title, fontSize = 16.sp)
+                Text(text = date, fontSize = 12.sp)
+            }
+        }
+        Text(
+            text = amount,
+            fontSize = 16.sp,
+            modifier = Modifier.align(Alignment.CenterEnd),
+            color = color
+        )
+    }
+}
+
+//@Composable
+//@Preview(showBackground = true)
+//fun HomeScreenPreview() {
+//    HomeScreen()
+//}
